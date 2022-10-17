739fb98401fef1fae4bc265d6e19f82270fd2d78
kipling <kiplingmarich@gmail.com>
2022-10-17 11:13:49 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-module.exports = function tiny(string) {
+
+ 
+  function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
   };
-  
+
+  function mpesaPayments(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+  };
+
+  function cardPayments(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+  };
+
+
+ 
+  function mobilePayouts(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+  };
+
+  function domesticPayouts(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+  };
+
+  module.exports = {
+    tiny,
+    mpesaPayments,
+    cardPayments,
+    mobilePayouts,
+    domesticPayouts,
+
+ }
